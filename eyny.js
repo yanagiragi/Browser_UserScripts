b0fd7ede8a8eba6dc9063e5958aa2dad5865a218
yanagiragi <qaz90473@gmail.com>
2017-02-18 04:27:30 +0800

    Update Solution
    Use Ajax Avoiding loading Page for several times

--- eyny.js
+++ eyny.js
@@ -1,42 +1,47 @@
 // ==UserScript==
-// @name         Eyny
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        http://*.eyny.com/index.php
-// @grant        none
+// @name        Eyny
+// @namespace   yanagiragi
+// @include     http://*.eyny.com/index.php
+// @version     1
+// @grant       none
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
 
 // Purpose : with entering "eyny.com", you will grant
 // 1. new url with www****.eyny.com, to avoid imgur.com blocking www**.eyny.com
 // 2. Already agreeed you are over 18 years old :)
 // 3. No longer access homepage twice if you're trying to access some forums
-
-function checkDomain(){
-    var loc = location.hostname.toString();     // domain
-    var str = loc.substr(loc.indexOf('www')); 
-    str = str.substr(0, str.indexOf('.'));      // get number of www**.eyny.com
-    
-    var org = loc.substr(loc.indexOf('.'));     // original postfix of '.eyny.com'
-
-    if(str.length <= 6){
-        str = str + (Math.floor(Math.random() * 100)).toString(); // new prefix : www****.eyny
-    }
-
-    str = str + org;
-    document.cookie = 'djAX_e8d7_agree=206;path=/;domain=.eyny.com'; // dealing r18 authentication
-
-    var path = location.pathname;
-    if(path.indexOf("/index.php") === 0){
-        path = '/forum.php?view=all'; // avoid access homepage twice
-    	console.log('detecting access index.php');
-        str += path;
-        document.location = 'http://' + str ;
-    }
-}
+const exclude = ['22']
 
 (function() {
     'use strict';
-    checkDomain();
+      
+      $('#hd').siblings()[2].innerHTML = "" // get rid of announcement since it may contain link to specific forum
+    
+      var forumId = []
+      $.ajax({
+          url : `http://${location.hostname}/forum.php?view=all`, 
+          async: false,
+          //headers: { 'Access-Control-Allow-Origin': '*' },
+          method : "get"
+      }).done(function(body){
+        $(body).find('[href]').filter(function(){
+          return this.href.match(/forum((-.)|(\.php\?view=all))/)
+        }).each(function(index, value){
+          forumId.push(value);
+        })
+      })
+      
+      $('[href]').filter(function(){ 
+        return this.href.match(/forum((-.)|(\.php\?view=all))/)
+      }).each(function(index, value){
+        if(value == 'forum.php?view=all'){
+           $(this).attr('href',`http://yrwww${Math.floor(Math.random() * 1000 + 1000)}.eyny.com/forum.php?mod=forumdisplay&fid=${forum[index]}&filter=author&orderby=dateline`)  
+        }
+        else{
+          var fid = value.href.substring( value.href.lastIndexOf('forum-')+6,value.href.lastIndexOf('-'))
+          if(exclude.indexOf(fid) == -1)
+           $(this).attr('href',`http://yrwww${Math.floor(Math.random() * 1000 + 1000)}.eyny.com/forum.php?mod=forumdisplay&fid=${fid}&filter=author&orderby=dateline`)  
+        }      
+      })   
 })();
