e79a74f506901068ec882aee5cc934d92c7c4a16
yanagiragi <qaz90473@gmail.com>
2017-04-06 15:17:41 +0800

    update google-Hestia.js

---
+++ google-Hestia.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        googleHestia
+// @namespace   yrGoogle
+// @include     https://www.google.com.tw/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+document.getElementById("hplogo").style["background-size"]= "";
+document.getElementById("hplogo").style["height"]= "298px";
+document.getElementById("hplogo").style["width"]= "321px";
+document.getElementById("hplogo").style["margin-top"]= "-93px";
+document.getElementById("hplogo").style["background"]= "url(https://i.imgur.com/QNV3ma8.png) no-repeat";
