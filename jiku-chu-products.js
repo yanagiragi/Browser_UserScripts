a0b29cafec17eb2a0fa48c72fdb47ce8aee5b06b
yanagiragi <qaz90473@gmail.com>
2018-06-07 02:09:44 +0800

    add jiku-chu-products.js

---
+++ jiku-chu-products.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         jiku-chu-products
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  make target=_blank of href great again
+// @author       Yanagiragi
+// @match        https://www.jiku-chu.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let targets = $('.title a');
+
+    for(let i = 0; i < targets.length; ++i){
+        let ele = targets[i];
+        let click = ele.getAttribute('onclick');
+        if(click){
+            let href = click.match(/\/products\/detail.php(\?)product_id=([0-9]*)/)[0];
+            ele.setAttribute('href', href);
+        }
+    }
+})();
