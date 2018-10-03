51174c8aed0395a617f770fc079f316f4299f4e8
yanagiragi <qaz90473@gmail.com>
2018-10-04 01:02:55 +0800

    update MediaFire.js

---
+++ MediaFire.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        MediaFire
+// @namespace   yanagiragi
+// @include     http://www.mediafire.com/file/*
+// @include     https://www.mediafire.com/file/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+// if error occurs with closing windows, try config firefox by accessing about:config
+var set;
+
+(function() {
+    'use strict';
+    var dl = document.querySelector('.download_link a');
+    dl = dl.getAttribute('href');
+    location.replace(dl); // start download
+    set = setInterval(closeWindows, 1000 * 3);
+})();
+
+function closeWindows(){
+    window.close();
+    clearInterval(set);
+}
