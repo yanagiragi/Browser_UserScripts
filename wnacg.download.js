fda5533b508ec82b6faea7b75a7e143b0b0534a8
yanagiragi <qaz90473@gmail.com>
2019-06-27 03:00:21 +0800

    Update wnacg.download.js

--- wnacg.download.js
+++ wnacg.download.js
@@ -1,27 +1,25 @@
 // ==UserScript==
 // @name         wnacgDownload
 // @namespace    yrWnacg
-// @version      1.1
+// @version      1.2
 // @description  Enhanced download of wnacg
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @author       Yanagiragi
 // @match        https://www.wnacg.org/photos-index-aid-*.html
-// @grant        none
+// @grant        GM_download
 // ==/UserScript==
 
 (function() {
     'use strict';
-    let btn = $('.downloadbtn')[0];
+    const title = $('#bodywrap h2').text();
+    const btn = $('.downloadbtn');
 
-
-    let title = $('#bodywrap h2')[0].innerHTML;
-    btn.download = encodeURIComponent(`${title}.zip`);
-
-    $.ajax(btn.href).done(function( data ) {
-        var el = $( '<div></div>' );
-        el.html(data);
-        let link = $('.down_btn', el)[0].href
-        btn.href = link
+    $.ajax(btn.attr('href')).done( data => {
+        const link = $('.down_btn', $(data)).attr('href')
+        btn.on('click', event => {
+            event.preventDefault();
+            GM_download(link, `${title}.zip`)
+        })
     });
 
 })();
