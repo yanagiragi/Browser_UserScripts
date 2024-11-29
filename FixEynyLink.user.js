490eb2dba307a89e18b564d1e2ea533840b3f8b7
yanagiragi <qaz90473@gmail.com>
2024-11-29 23:40:03 +0800

    fix: Add FixLinkEyny

---
+++ FixEynyLink.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Fix Eyny Link
+// @namespace    Yr
+// @version      1.1
+// @description  Fix Eyny Link
+// @match        https://*.eyny.com/forum.php?mod=forumdisplay&fid=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=eyny.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const regex = /forum\.php\?mod=viewthread&tid=([0-9]*)&/
+    const links = [...document.querySelectorAll('a.xst'), ...document.querySelectorAll('.ptn > a') ]
+    for(const link of links) {
+        const href = link.href
+        const match = href.match(regex)
+        if (match) {
+            link.href = `https://www21.eyny.com/thread-${match[1]}-1-BX4TQDHP.html`
+            console.log(`Replace ${href} to ${link.href}`)
+        }
+    }
+})();
