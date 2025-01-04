b1620d4c78dcb83bd2e1e91f6cdead4df7fcf0fa
yanagiragi <qaz90473@gmail.com>
2025-01-04 15:30:36 +0800

    refactor: Add http support

--- FixEynyLink.user.js
+++ FixEynyLink.user.js
@@ -1,18 +1,19 @@
 // ==UserScript==
 // @name         Fix Eyny Link
 // @namespace    Yr
-// @version      1.1
+// @version      1.2
 // @description  Fix Eyny Link
+// @match        http://*.eyny.com/forum.php?mod=forumdisplay&fid=*
 // @match        https://*.eyny.com/forum.php?mod=forumdisplay&fid=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=eyny.com
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     const regex = /forum\.php\?mod=viewthread&tid=([0-9]*)&/
-    const links = [...document.querySelectorAll('a.xst'), ...document.querySelectorAll('.ptn > a') ]
-    for(const link of links) {
+    const links = [...document.querySelectorAll('a.xst'), ...document.querySelectorAll('.ptn > a')]
+    for (const link of links) {
         const href = link.href
         const match = href.match(regex)
         if (match) {
